bf74767d9a75681bc63f9b1dbcaa6326dca414e8
David Whitlock <david.m.whitlock@gmail.com>
2024-03-06 19:00:50 -0800

    Respond to the user clicking on "=" by summing together the two operands.

--- apptbook-android/app/src/main/java/edu/pdx/cs410J/whitlock/FirstFragment.java
+++ apptbook-android/app/src/main/java/edu/pdx/cs410J/whitlock/FirstFragment.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -29,10 +30,17 @@ public class FirstFragment extends Fragment {
     public void onViewCreated(@NonNull View view, Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
 
-//        binding.buttonFirst.setOnClickListener(v ->
-//                NavHostFragment.findNavController(FirstFragment.this)
-//                        .navigate(R.id.action_FirstFragment_to_SecondFragment)
-//        );
+        binding.equals.setOnClickListener(v -> {
+            String leftOperand = binding.leftOperand.getText().toString();
+            String rightOperand = binding.rightOperand.getText().toString();
+
+            int left = Integer.parseInt(leftOperand);
+            int right = Integer.parseInt(rightOperand);
+            int sum = left + right;
+
+            binding.sum.setText(String.valueOf(sum));
+        }
+        );
     }
 
     @Override
